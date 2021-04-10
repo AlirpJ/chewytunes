07c7f9014edf74458a2d5c51d7b6be5c081457b9
YasiTL <yasaman.most@gmail.com>
2021-04-09 23:19:59 -0500

    install react paper js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { PaperContainer, Circle, Layer } from '@psychobolt/react-paperjs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Shapes = () => <Circle center={[120, 50]} radius={35} fillColor="#00FF00" />;
+
+const App = (props) => (
+  <div>
+    <PaperContainer {...props}>
+      <Circle center={[80, 50]} radius={35} fillColor="red" />
+      <Layer>
+        <Shapes />
+      </Layer>
+    </PaperContainer>
+  </div>
+);
 
 export default App;
