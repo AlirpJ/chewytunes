fb84730c2c55f28d1f8aaf85e7e3030eb32f466b
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2021-04-09 23:28:05 -0500

    Messing around with variables in example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { PaperContainer, Circle, Layer } from '@psychobolt/react-paperjs'
+import React from "react";
+import { PaperContainer, Circle, Layer } from "@psychobolt/react-paperjs";
 
-const Shapes = () => <Circle center={[120, 50]} radius={35} fillColor="#00FF00" />;
+const Shapes = () => (
+  <Circle center={[120, 50]} radius={35} fillColor="#00FF00" />
+);
 
 const App = (props) => (
   <div>
     <PaperContainer {...props}>
-      <Circle center={[80, 50]} radius={35} fillColor="red" />
+      <Circle center={[80, 75]} radius={125} fillColor="red" />
       <Layer>
         <Shapes />
       </Layer>
