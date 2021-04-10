6f80ff31c859d589339d45712b5d3657a2bcfa0b
alirpJ <jprila@hawk.iit.edu>
2021-04-10 14:31:28 -0500

    modified:   src/Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Dashboard() {
+export default function Dashboard({code}) {
     return (
         <div>
             This is a dashboard. You logged in with spotify! 
+            {code}
         </div>
     )
 }
