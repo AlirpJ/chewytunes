554f21c9d736eb6096191e954192a3cbd87b2236
YasiTL <yasaman.most@gmail.com>
2021-04-10 15:32:36 -0500

    home page complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
 import Wave from 'react-wavify';
 import './App.css';
-
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+import "./my-button.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
-
   render() {
     return (
-      <div >
-      <Wave style={{transform: [{ rotate: '90deg' }]}}
-        fill='#f79902'
-        paused={false}
-        options={{
-          height: 20,
-          amplitude: 20,
-          speed: 0.25,
-          points: 5
-        }}
-  />
+      <div class="background">
+        <div class="top-half">
+        <div class="top-half-actions">
+          <h1 class="top"> Find me foods that go with my favorite tunes </h1>
+          <AwesomeButton type="secondary"><FontAwesomeIcon icon={faChevronUp} size='2x'/></AwesomeButton>
+        </div>
+        </div>
+        <div class="background">
+          <div class="on-top">
+            <h1 class="bottom"> Find me tunes that will go with my current meal </h1>
+            <AwesomeButton type="primary"><FontAwesomeIcon icon={faChevronDown} size='2x'/></AwesomeButton>
+          </div>
+          <div class="bottom-half">
+            <Wave fill='#4C753F'
+              class='wave'
+              paused={false}
+              options={{
+                height: 10,
+                amplitude: 20,
+                speed: 0.25,
+                points: 5
+              }}
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- src/b.js
+++
@@ -1,9 +0,0 @@
-import { paper } from 'paper';
-
-console.log('b loaded');
-
-let circle = new paper.Path.Circle({
-  center: paper.view.center,
-  radius: 50,
-  fillColor: 'blue'
-});
