6de1ca9f2fceb7fa455b98872cecec1c5f0819c9
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2021-04-10 12:51:57 -0500

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Container } from "react-bootstrap"
 
+var client_id = '992f2aaaf07c467fb5fd757f91391847'; // Your client id
+var redirect_uri = 'http://localhost:3000'; // Your redirect uri
+
 // SCOPES NEED TO BE MODIFIED FOR OUR USE
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id="+client_id+"&response_type=code&redirect_uri="+redirect_uri+"&scope=streaming%20user-read-email"
     +"%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
