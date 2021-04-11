7624e7067d76c3b3dbfe24e422911154d0c9ef65
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2021-04-11 07:07:10 -0500

    Final Pathing done + fixing Auth AGAIN

--- src/BottomOption.js
+++ src/BottomOption.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Auth from './auth.js';
+import React from "react";
+import Auth from "./Auth.js";
 
 export default function BottomOption() {
-    return (
-        <div class="bottom-option">
-            <Auth/>
-        </div>
-    )
+  return (
+    <div class="bottom-option">
+      <Auth />
+    </div>
+  );
 }
