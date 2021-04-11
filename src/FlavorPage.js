56353e4d5042dce8fe849515a6bf969a392ffe81
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2021-04-11 06:32:44 -0500

    Added dictionary to store values inputted by user

--- src/FlavorPage.js
+++ src/FlavorPage.js
@@ -7,6 +7,55 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
+var dict = {
+  Crunchy: false,
+  Chewy: false,
+  Spicy: false,
+  Salty: false,
+  Bitter: false,
+  Sweet: false,
+};
+
+function UpdateDict(flavorSelected) {
+  switch (flavorSelected) {
+    case "crunchy":
+      dict.Crunchy = !dict.Crunchy;
+      if (dict.Chewy === true && dict.Crunchy === true) {
+        dict.Chewy = false;
+      }
+      break;
+    case "chewy":
+      dict.Chewy = !dict.Chewy;
+      if (dict.Chewy === true && dict.Crunchy === true) {
+        dict.Crunchy = false;
+      }
+      break;
+    case "spicy":
+      dict.Spicy = !dict.Spicy;
+      if (dict.Salty === true && dict.Spicy === true) {
+        dict.Salty = false;
+      }
+      break;
+    case "salty":
+      dict.Salty = !dict.Salty;
+      if (dict.Salty === true && dict.Spicy === true) {
+        dict.Spicy = false;
+      }
+      break;
+    case "bitter":
+      dict.Bitter = !dict.Bitter;
+      if (dict.Sweet === true && dict.Bitter === true) {
+        dict.Sweet = false;
+      }
+      break;
+    case "sweet":
+      dict.Sweet = !dict.Sweet;
+      if (dict.Sweet === true && dict.Bitter === true) {
+        dict.Bitter = false;
+      }
+      break;
+  }
+}
 function FlavorPage(props) {
   return (
     <div class="flavorPage">
@@ -14,33 +63,41 @@ function FlavorPage(props) {
       <body class="flavor-body">
         <div class="container">
           <div class="item itemA">
-            <AwesomeButton type="primary">Crunchy</AwesomeButton>
+            <AwesomeButton type="primary" onPress={() => UpdateDict("crunchy")}>
+              Crunchy
+            </AwesomeButton>
           </div>
           <div class="item itemB">
             <p class="flavor-or">Or</p>
           </div>
           <div class="item itemC">
-            <AwesomeButton type="primary" border-radius={25}>
+            <AwesomeButton type="primary" onPress={() => UpdateDict("chewy")}>
               Chewy
             </AwesomeButton>
           </div>
           <div class="item itemA">
-            <AwesomeButton type="primary">Spicy</AwesomeButton>
+            <AwesomeButton type="primary" onPress={() => UpdateDict("spicy")}>
+              Spicy
+            </AwesomeButton>
           </div>
           <div class="item itemB">
             <p class="flavor-or">Or</p>
           </div>
           <div class="item itemC">
-            <AwesomeButton type="primary">Salty</AwesomeButton>
+            <AwesomeButton type="primary" onPress={() => UpdateDict("salty")}>
+              Salty
+            </AwesomeButton>
           </div>
           <div class="item itemA">
-            <AwesomeButton type="primary">Bitter</AwesomeButton>
+            <AwesomeButton type="primary" onPress={() => UpdateDict("bitter")}>
+              Bitter
+            </AwesomeButton>
           </div>
           <div class="item itemB">
             <p class="flavor-or">Or</p>
           </div>
           <div class="item itemC">
-            <AwesomeButton type="primary" button-default-border-radius={25}>
+            <AwesomeButton type="primary" onPress={() => UpdateDict("sweet")}>
               Sweet
             </AwesomeButton>
           </div>
