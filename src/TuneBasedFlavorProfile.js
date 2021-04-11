0b0c3adfbfe685c13b508537ff3c3b59ea0f8ad7
Aman <aluqman@hawk.iit.edu>
2021-04-11 07:12:22 -0500

    fixed some typos

--- src/TuneBasedFlavorProfile.js
+++ src/TuneBasedFlavorProfile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./FlavorBasedTuneCurration.css";
+import "./TuneBasedFlavorProfile.css";
 import SamplePhoto from "./logo192.png";
 import { mapStatsToFlavors, getUserTopSongs, getRandom } from "./JSonParse"
 const SpotifyWebApi = require('spotify-web-api-node')
@@ -32,14 +32,20 @@ function TuneBasedFlavorProfile(props) {
   
   return (
     <div class="TunesBackground">
-      <h1 class="tunestop">Here are your delicious tunes</h1>
+      <h1 class="tunestop">Here are your food recommendations!</h1>
       <p>{flavorStats[0]}</p>
-      <p> How about trying Panera's Clasic, Broccoli Cheddar Mac & Cheese! </p>
-      <ul>
+      <p>How about trying Panera's Classic, Broccoli Cheddar Mac & Cheese! </p>
+      <p>(Here are the tracks we based your food selection on, in case you were curious):</p>
+      <div class="container">
         {top10.map((value, index) => {
-          return <li key={index}>{value}</li>
+          return (
+            <div class="unit">
+              <img src={value[0]}></img>
+              <p key={index}>{value[1] + " - " + value[2]}</p>
+            </div>
+          );
         })}
-      </ul>
+      </div>
     </div>
   );
 }
