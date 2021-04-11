bf2e547396aa0d633b1dfd59a21a711250eaa59e
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2021-04-11 02:15:21 -0500

    TunesPage fix plus auth once again

--- src/auth.js
+++ src/auth.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Login from "./Login"
-import Dashboard from "./Dashboard"
+import React from "react";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 export default function Auth() {
-  var access_token = new URLSearchParams(window.location.hash).get('#access_token');
-  localStorage.setItem('spotifyAccessToken', access_token);
+  var access_token = new URLSearchParams(window.location.hash).get(
+    "#access_token"
+  );
+  localStorage.setItem("spotifyAccessToken", access_token);
   console.log("access token", access_token);
 
-  return (
-      access_token ? <Dashboard access_token = {access_token} /> : <Login />
-  )
+  return access_token ? <Dashboard access_token={access_token} /> : <Login />;
 }
